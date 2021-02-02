853df22f0a8cb4dad347ec4340e2fdf92eeab16e
Alex Damascena <52205263+apfdamascena@users.noreply.github.com>
2021-02-02 16:08:04 -0300

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
@@ -6,57 +6,80 @@ typedef long long ll;
 #define pf push_front
 #define x first
 #define y second
+#define endl "\n"
 const int INF = 0x3f3f3f3f;
-//g++-9 -std=c++17 main.cpp -o main && time ./main < input.txt
 
-const int maxn = 100005;
-vector<int> baldes(maxn);
-vector<int> tree(4*maxn);
 
-void build(int pos, int i, int j){
-	int esq = 2*pos;
-	int dir = 2*pos+1;
-	int mid = (i+j)/2;
-	
-	if(i == j) tree[pos] = balde[i];
-	else {
-		tree[pos] = 0;
-		build(esq, i, mid);
-		build(dir,mid+1,j);
-		tree[pos] = tree[esq] + tree[dir];
+//g++ -std=c++17 main.cpp -o main && time ./main < input.txt
+
+
+const int N = 1e5+5;
+int tree[4*N];
+int v[N];
+
+void build(int p, int l, int r){
+	if( l == r ){
+		tree[p] = v[r];
+	} else {
+		int m = (l+r)/2, lc = 2*p, rc = lc+1;
+		build(lc, l, m);
+		build(rc, m+1, r);
+		tree[p] = min(tree[lc], tree[rc]);
 	}
 }
 
-void update(int pos, int i, int j, int x, int value){
-	int esq = 2*pos;
-	int dir = 2*pos+1;
-	int mid = (i+j)/2;
-	
-	if(i==j) tree[pos] += value;
-	else {
-		tree[pos] += value;
-		if(x <= mid) update(esq, i, mid, x, value);
-		else update(dir, mid+1, j, x, value);
+void update(int p, int l, int r, int i, int x){
+	if(l == r){
+		tree[p] = x;
+ 	} else {
+		int m = (l+r)/2, lc = 2*p, rc = lc+1;
+		if(i <= m){
+			update(lc, l, m, i, x);
+		} else {
+			update(rc, m+1, r, i, x);
+		}
+		tree[p] = min(tree[lc], tree[rc]);
 	}
 }
 
-int query(int pos, int i, int j, int l, int r){
-	int esq = 2*pos;
-	int dir = 2*pos+1;
-	int mid = (j+i)/2;
-	int ret;
-	
-	if(j < l || i > r) ret = 0;
-	else if(i >= l && j <= r) ret = tree[pos];
-	else {
-		ret = 0;
-		ret += query(esq, i, mid, l, r);
-		ret +=  query(dir, mid+1, j, l, r);
+int query(int p, int l, int r, int ql, int qr){
+	if( ql <= l && r <= qr){
+		return tree[p];
+	} else {
+		int m = (l+r)/2, lc = 2*p, rc = lc+1;
+		if(qr <= m){
+			return query(lc, l, m, ql, qr);
+		} else if (ql > m){
+			return query(rc, m+1, r, ql, qr);
+		} else {
+			return min(query(lc, l, m, ql, qr), query(rc, m+1, r, ql, qr));
+		}
 	}
-	return ret;
+	
 }
 
-void solve(){}
+int query2(int p, int l, int r, int ql, int qr){
+	if( ql <= l && r <= qr){
+		return tree[p];
+	} else if(qr < l || r < ql){
+		return INF;
+	} else {
+		int m = (l+r)/2, lc= 2*p, rc = lc+1;
+		return min(query2(lc, l, m, ql, qr), query2(rc, m+1, r, ql, qr));
+	}
+}
+
+void solve(){
+	int n; cin >> n;	
+	for(int i=1; i<=n; i++) cin >> v[i];
+	
+	//1-based
+	build(1,1,n);
+	
+	
+	for(int i=1; i<= 4*n; i++) cout << tree[i] <<  " ";
+
+}
 
 int main(){
 	ios::sync_with_stdio(0);
@@ -64,3 +87,4 @@ int main(){
 	solve();
 	return 0;
 }
+
